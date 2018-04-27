3245503d2700b34853e972acae6632ec5109103f
yinde <yinde@hnjing.com>
2018-04-27 09:31:53 +0800

    a

--- trunk/src/main/java/com/jing/controller/UserController.java
+++ trunk/src/main/java/com/jing/controller/UserController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.pagehelper.PageHelper;
@@ -21,12 +22,21 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
-	@RequestMapping("/test/{id}")
+	@RequestMapping(value ="/test/{id}", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
 	public Object test(HttpServletRequest request, HttpServletResponse response,
 			@PathVariable("id") Integer id){
 		PageHelper.startPage(1, 20,"sex desc,id asc");
 		List<User> userlist = userService.queryUserList();
 		PageInfo<User> pageInfo = new PageInfo<>(userlist);  
+		request.getSession().setAttribute("user", userlist);
 		return pageInfo;
+	}
+	
+	@RequestMapping(value ="/testlist", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
+	public Object testSession(HttpServletRequest request, HttpServletResponse response){
+		@SuppressWarnings("unchecked")
+		List<User> list = (List<User>)request.getSession().getAttribute("user");
+		System.out.println(list.size());
+		return null;
 	}
 }
